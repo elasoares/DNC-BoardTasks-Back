a48275f947a402aa68584e893e07c7c316fe15f6
Elaine Soares <131168713+elasoares@users.noreply.github.com>
2025-02-18 16:05:34 -0300

    Instalou bibliotecas do npm e iniciou o projeto com express

---
+++ swagger/autoGenDoc.js
@@ -0,0 +1,38 @@
+const { version } = require('mongoose');
+const mongooseToSwagger = require('mongoose-to-swagger');
+const swaggerAutogen = require('swagger-autogen')({
+    openapi: '3.0.0',
+    language: 'pt-BR',
+});
+
+const outputFile = './swagger_output.json';
+const endpointsFiles = ['../index.js'];
+
+
+let doc = {
+    info: {
+        version: "1.0.0",
+        title: "API dp BoardTasks",
+        description: "Documentação da API do BoardTasks."
+    },
+    servers: [
+        {
+
+            url: "http://localhost:4000",
+            description: "Servidor localhost"
+        },
+        {
+            url: "https://boardtasks-back.vercel.app/",
+            description: "Servidor de produção."
+        }
+    ],
+    consumes: ['application/json'],
+    produces: ['application/json']
+}
+
+swaggerAutogen(outputFile, endpointsFiles, doc).then(()=>{
+    console.log("Documentação do swagger gerada encontra-se no arquivo em: " + outputFile);
+    if(process.env.NODE_ENV !== 'test'){
+        require("../index.js");
+    }
+})
