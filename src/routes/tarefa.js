e9a525de7437b4a1c2048df728b96c158a90b559
Elaine Soares <131168713+elasoares@users.noreply.github.com>
2025-03-16 17:57:14 -0300

    Criado api de edição de tarefa

--- src/routes/tarefa.js
+++ src/routes/tarefa.js
@@ -11,8 +11,8 @@ router.post('/criar', authUser, connectMongoDB, async function(req, res, next) {
     // #swagger.tags = ['Tarefa']
 
     let { posicao, titulo, descricao, status, dataEntrega } = req.body;
-    const usuarioCriador = req.usuarioJwt.id;
-    const respostaDB = await EsquemaTarefa.create({ posicao, titulo, descricao, status, dataEntrega, usuarioCriador });
+    const usuarioLogado = req.usuarioJwt.id;
+    const respostaDB = await EsquemaTarefa.create({ posicao, titulo, descricao, status, dataEntrega, usuarioLogado });
 
     res.status(200).json({
       status: "Ok",
@@ -20,6 +20,38 @@ router.post('/criar', authUser, connectMongoDB, async function(req, res, next) {
       resposta: respostaDB
     });
 
+
+  }catch(error){
+    return tratarErros(res, error);
+  }
+});
+
+router.put('/editar/:id', authUser, connectMongoDB, async function(req, res, next) {
+  try{
+    // #swagger.tags = ['Tarefa']
+    let idTarefa = req.params.id;
+
+    let { posicao, titulo, descricao, status, dataEntrega } = req.body;
+
+    const usuarioLogado = req.usuarioJwt.id;
+
+    const checkTarefa = await EsquemaTarefa.findOne({_id: idTarefa, usuarioCriador: usuarioLogado})
+
+    if(!checkTarefa){
+      throw new Error("Tarefa não encontrada ou pertence a outro usuário.")
+    }
+
+    const tarefaAtualizada = await EsquemaTarefa.updateOne({_id: idTarefa},{ posicao, titulo, descricao, status, dataEntrega });
+    if(tarefaAtualizada?.modifiedCount > 0){
+      const dadostarefa = await EsquemaTarefa.findOne({_id: idTarefa}).populate('usuarioCriador');
+      res.status(200).json({
+        status: "Ok",
+        statusMensagem: "Tarefa atualizada com sucesso!",
+        resposta: dadostarefa
+      });
+  
+    }
+    
 
   }catch(error){
     return tratarErros(res, error);
